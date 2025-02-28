904aa5789ad612af546e95e6beb8b2b6feee147d
Prerak siddhpura <prerak.siddhpura@gmail.com>
2025-03-01 02:29:04 +0530

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,15 +6,18 @@ export const revalidate = 0;
 export default async function HomePage() {
 
   const images = await db.query.images.findMany({
-    orderBy: (modal, {desc}) => desc(modal.id),
+    // orderBy: (modal, {desc}) => desc(modal.id),
   });
+
+  // console.log(images);
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">
+        {images.map((image) => (<div key={image.id}>{image.name}</div>))}
         {[...images, ...images, ...images].map((image, index) => (
-          <div key={`image-${index}`} className="w-56 flex flex-col pt-5">
+          <div key={`image${index}`} className="w-56 flex flex-col pt-5">
             <img src={image.url} alt={`Image ${index + 1}`} />
-            <div>{image.name}</div>
+           
           </div>
         ))}
       </div>

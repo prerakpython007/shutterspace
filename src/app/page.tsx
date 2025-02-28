f361125771404a4cfce96b2e0b90809b7958eb3e
Prerak siddhpura <prerak.siddhpura@gmail.com>
2025-02-28 19:52:55 +0530

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,28 @@
 import Link from "next/link";
 
+const mockUrls = [
+  "https://l0jk0814aw.ufs.sh/f/cyUjMXWL2FnSd67VI9CDI8h45aHy0CqBGSbeuDZn7prwYxgX",
+  "https://l0jk0814aw.ufs.sh/f/cyUjMXWL2FnSd67VI9CDI8h45aHy0CqBGSbeuDZn7prwYxgX",
+  "https://l0jk0814aw.ufs.sh/f/cyUjMXWL2FnSd67VI9CDI8h45aHy0CqBGSbeuDZn7prwYxgX",
+  "https://mryx17lej8.ufs.sh/f/HzaylkhOXuVEHflYpghOXuVEA1Z76qdCzj5rsvB8WTPYfx2t",
+  "https://mryx17lej8.ufs.sh/f/HzaylkhOXuVEuqk3SgEE2xwpnG5XgqVL1vBt6sdym78WhZHl",
+]
+
+const mockImages = mockUrls.map((url , index)=> ({
+  id: index + 1,
+  url,
+}))
+
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      yuyu
+    <main className="">
+      <div className="flex flex-wrap gap-4">
+        {mockImages.map((image) => (
+          <div key={image.id} className="w-56 p-5">
+            <img src={image.url}  alt="img" />
+          </div>
+        ))}
+      </div>
     </main>
   );
 }
